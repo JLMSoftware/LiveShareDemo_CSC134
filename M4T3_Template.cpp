97ac576051e7f89ef5763526f0e498721938c349
JLMSoftware <mitcheljprogramming@gmail.com>
2024-11-04 10:57:08 -0500

    Create M4T3_Template.cpp

---
+++ M4T3_Template.cpp
@@ -0,0 +1,24 @@
+//Put your own header
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int length = 7;
+
+  int rows = 7;
+
+  for (int r = 0; r < rows; r++) {
+
+    for (int l = 0; l < length; l++) {
+
+      cout << "👽" <<  " ";
+    }
+
+    cout << endl;
+
+    
+  }
+  
+  cout << "[PUT OUTPUT MESSAGE HERE]" << endl;
+}
